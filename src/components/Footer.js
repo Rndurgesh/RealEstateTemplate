584d4841a1a65cde05962317a5277b75782f7288
Rndurgesh <96943241+Rndurgesh@users.noreply.github.com>
2023-04-25 15:51:29 +0530

    first deploy

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="bg-black py-8 text-center text-white">
+      <div className="container mx-auto">
+        Copyrights reserved to DURGESH SOLANKE...
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/pages/Home.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+// importing components
+import Banner from "../components/Banner";
+import HouseList from "../components/HouseList";
+
+const Home = () => {
+  return (
+    <div className="min-h-[1800px] py-10">
+      <Banner />
+      <HouseList />
+    </div>
+  );
+};
+
+export default Home;
